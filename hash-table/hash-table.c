3e12bd7423019cd8abb290dd2efee9b6908b6d27
Gabriel Felipe Kuhn Schiestl <gabriel.schiestl25@hotmail.com>
2026-03-09 16:31:05 -0300

    feat: add table printing and resizing

--- hash-table/hash-table.c
+++ hash-table/hash-table.c
@@ -1,17 +1,22 @@
 #include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define INITIAL_TABLE_SIZE 50
+#define MAX_KEY_SIZE 50
+#define TABLE_RESIZE_NEEDED 90
 
 typedef struct Entry {
     char* key;
     void* value;
     int* ttl;
-    Entry* next;
+    struct Entry* next;
 } Entry;
 
-Entry *table[INITIAL_TABLE_SIZE];
-int table_size = INITIAL_TABLE_SIZE;
+Entry **table;
+int table_size;
+int element_count;
 
 int hash(const char *key) {
     int hash_value = 0;
@@ -23,14 +28,37 @@ int hash(const char *key) {
     return hash_value;
 }
 
-int8_t table_insert(const char* key, Entry* value) {
+int resize_needed() {
+    return ((element_count / table_size) * 100) >= TABLE_RESIZE_NEEDED ? 0 : -1; 
+}
+
+int resize_table() {
+    int new_table_size = table_size * 2;
+
+    void* tmp = realloc(table, new_table_size);
+    if(tmp == NULL) {
+        return -1;
+    }
+
+    table_size = new_table_size;
+    table = tmp;
+
+    return 0;
+}
+
+int8_t table_insert(const char* key, Entry *value) {
     if(strlen(key) == 0) {
         return -1;
     }
 
+    element_count++;
     int index = hash(key);
     value->next = table[index];
     table[index] = value;
+
+    if(resize_needed() != -1) {
+        resize_table();
+    }
 
     return 0;
 }
@@ -66,7 +94,7 @@ int8_t table_delete(const char* key) {
 
 Entry* table_get(const char* key) {
     if (strlen(key) == 0) {
-        return -1;
+        return NULL;
     }
 
     int index = hash(key);
@@ -79,11 +107,51 @@ Entry* table_get(const char* key) {
         current = current->next;
     }
 
-    return -1;
+    return NULL;
+}
+
+void print_table() {
+    for(int i = 0; i < table_size; i++) {
+        Entry *curr = table[i];
+        if(curr == NULL) {
+            printf("%d ----\n", i);
+            continue;
+        }
+
+        printf("%d - ", i);
+        while(curr) {
+            printf("%s\t\t", curr->key);
+            curr = curr->next;
+        }
+        printf("\n");
+    }
+    printf("--------------\n");
+}
+
+Entry* new_entry(const char* key, void* value, int *ttl) {
+    Entry *e1 = malloc(sizeof(Entry));
+    char *e1_key = strndup(key, MAX_KEY_SIZE);
+
+    e1->key = e1_key;
+    e1->value = value;
+    e1->ttl = ttl;
+
+    return e1;
 }
 
 int main() {
+    table = calloc(INITIAL_TABLE_SIZE, sizeof(Entry*));
+    table_size = INITIAL_TABLE_SIZE;
+    print_table();
     
+    Entry *e1 = new_entry("Teste", (void*)1, NULL);
+    Entry *e2 = new_entry("bw", (void*)1, NULL);
+    
+    table_insert(e1->key, e1);
+    table_insert(e2->key, e2);
+    print_table();
+    table_delete(e2->key);
+    print_table();
 
     return 0;
 }
